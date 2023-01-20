eec7e2d0fed9b895483b41e88cd1ff2dc097f21e
Andranik Khachatryan <andranik.khachatryan0589@gmail.com>
2023-01-21 01:04:59 +0300

    usermodels

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import Jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+
+import UserModel from './models/User.js';
+
+mongoose.set('strictQuery', true);
+mongoose
+  .connect('mongodb+srv://admin:wwwwww@cluster0.uhcjino.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+  });
+
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('server ok');
+});
---
+++ validations/auth.js
@@ -0,0 +1,8 @@
+import { body } from 'express-validator';
+
+export const registerValidation = [
+  body('email').isEmail(),
+  body('email').isLength({ min: 5 }),
+  body('fullName').isLength({ min: 3 }),
+  body('avatarUrl').optional().isURL(),
+];
