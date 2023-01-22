52943357c88e4b20a8923f93f5011becb2a94780
Andranik Khachatryan <andranik.khachatryan0589@gmail.com>
2023-01-22 16:53:35 +0300

    post

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from 'express';
-import Jwt from 'jsonwebtoken';
+
 import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
 
-import { registerValidation } from './validations/auth.js';
+import { registerValidation, loginValidation } from './validations.js';
 
-import UserModel from './models/User.js';
+import checkAuth from './utils/checkAuth.js';
+import { register, login, getMe } from './controllers/UserController.js';
 
 mongoose.set('strictQuery', true);
 mongoose
-  .connect('mongodb+srv://admin:wwwwww@cluster0.uhcjino.mongodb.net/?retryWrites=true&w=majority')
+  .connect(
+    'mongodb+srv://admin:wwwwww@cluster0.uhcjino.mongodb.net/blog?retryWrites=true&w=majority',
+  )
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
@@ -17,23 +19,11 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+app.post('/auth/login', loginValidation, login);
 
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash: req.body.email,
-  });
+app.post('/auth/register', registerValidation, register);
 
-  res.json({
-    success: true,
-  });
-});
+app.get('/auth/me', checkAuth, getMe);
 
 app.listen(4444, (err) => {
   if (err) {
